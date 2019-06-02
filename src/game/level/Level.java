1d56bc777d4234642914c12d103d4b61fcc7287c
Tony Zaliznyi (Stark) <lifegamecom1@gmail.com>
2019-06-02 12:10:15 +0300

    fixed tiles map

--- src/game/level/Level.java
+++ src/game/level/Level.java
@@ -40,7 +40,7 @@ public class Level {
                 TILE_SCALE), TILE_IN_GAME_SCALE, TileType.BRICK_BLOCK_BLUE));
         tiles.put(TileType.COIN_BLOCK, new Tile(atlas.cut(24 * TILE_SCALE, 0 * TILE_SCALE, TILE_SCALE,
                 TILE_SCALE), TILE_IN_GAME_SCALE, TileType.COIN_BLOCK));
-        tiles.put(TileType.COIN, new Tile(atlas.cut(23 * TILE_SCALE, 1 * TILE_SCALE, TILE_SCALE,
+        tiles.put(TileType.COIN, new Tile(atlas.cut(24 * TILE_SCALE, 1 * TILE_SCALE, TILE_SCALE,
                 TILE_SCALE), TILE_IN_GAME_SCALE, TileType.COIN));
 
         tiles.put(TileType.BUSH_CENTER, new Tile(atlas.cut(12 * TILE_SCALE, 11 * TILE_SCALE, TILE_SCALE,
@@ -50,11 +50,11 @@ public class Level {
         tiles.put(TileType.BUSH_RIGHT, new Tile(atlas.cut(13 * TILE_SCALE, 11 * TILE_SCALE, TILE_SCALE,
                 TILE_SCALE ), TILE_IN_GAME_SCALE, TileType.BUSH_RIGHT));
 
-        tiles.put(TileType.PLATFORM_GRASS_LEFT, new Tile(atlas.cut(8 * TILE_SCALE, 5 * TILE_SCALE, TILE_SCALE,
+        tiles.put(TileType.PLATFORM_GRASS_LEFT, new Tile(atlas.cut(5 * TILE_SCALE, 8 * TILE_SCALE, TILE_SCALE,
                 TILE_SCALE ), TILE_IN_GAME_SCALE, TileType.PLATFORM_GRASS_LEFT));
-        tiles.put(TileType.PLATFORM_GRASS_CENTER, new Tile(atlas.cut(8 * TILE_SCALE, 6 * TILE_SCALE, TILE_SCALE,
+        tiles.put(TileType.PLATFORM_GRASS_CENTER, new Tile(atlas.cut(6 * TILE_SCALE, 8 * TILE_SCALE, TILE_SCALE,
                 TILE_SCALE ), TILE_IN_GAME_SCALE, TileType.PLATFORM_GRASS_CENTER));
-        tiles.put(TileType.PLATFORM_GRASS_RIGHT, new Tile(atlas.cut(8 * TILE_SCALE, 7 * TILE_SCALE, TILE_SCALE,
+        tiles.put(TileType.PLATFORM_GRASS_RIGHT, new Tile(atlas.cut(7 * TILE_SCALE, 8 * TILE_SCALE, TILE_SCALE,
                 TILE_SCALE ), TILE_IN_GAME_SCALE, TileType.PLATFORM_GRASS_RIGHT));
 
         tiles.put(TileType.CLOUD_UP_LEFT, new Tile(atlas.cut(0 * TILE_SCALE, 20 * TILE_SCALE, TILE_SCALE, TILE_SCALE),
