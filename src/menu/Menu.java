d23ffd48b6441568b33b4ab063aa92ddcf4e68a7
Tony Zaliznyi (Stark) <lifegamecom1@gmail.com>
2019-06-03 22:29:47 +0300

    Added basic music player

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,9 +1,14 @@
 package menu;
 
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
 import levelEditor.LevelEditor;
 import utils.ResourceLoader;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -17,8 +22,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class Menu extends JFrame {
     private JPanel root;
@@ -75,7 +83,24 @@ public class Menu extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setUndecorated(true);
+        setMusic();
         setVisible(true);
+    }
+
+    private void setMusic() {
+        String songName = "overWorld.wav";
+        String pathToMp3 = "res/sound/environment/" + songName;
+
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(
+                    new File(pathToMp3).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
     }
 
     private void setUp() {
